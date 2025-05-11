44305ff2ef17e00b975e78e7b2ccfe15292950c4
Atharva Vaze <vaze.atharva18@gmail.com>
2025-05-10 19:38:10 -0500

    sentiment.py: notes added

--- utils/sentiment.py
+++ utils/sentiment.py
@@ -1,12 +1,12 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
+# last notes done 05-10-25 7:37 PM CST
 class SentimentAnalyzer:
     def __init__(self):
-        self._vd = SentimentIntensityAnalyzer()
+        self._vd = SentimentIntensityAnalyzer() #analyzer object
 
     def sentiment(self, text: str) -> float:
-        return self._vd.polarity_scores(text)['compound']
+        return self._vd.polarity_scores(text)['compound'] # returns neg (negative), neu (neutral), pos (positive), compound (composite) values
 
-    def hype_score(self, likes: int, retweets: int, text: str) -> float:
-        s = self.sentiment(text)
-        return (likes * 0.6 + retweets * 1.2) * s
+    def hype_score(self, likes: int, retweets: int, text: str) -> float: # returns a float; parameters are parts of a tweet and associated type
+        s = self.sentiment(text) # compound score from earlier
+        return (likes * 0.6 + retweets * 1.2) * s # formula
