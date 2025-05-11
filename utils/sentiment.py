59c8d819b61be530632c87e49e24724a163a6615
Atharva Vaze <vaze.atharva18@gmail.com>
2025-05-10 19:49:26 -0500

    Update sentiment.py

--- utils/sentiment.py
+++ utils/sentiment.py
@@ -1,12 +1,14 @@
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-# last notes done 05-10-25 7:37 PM CST
-class SentimentAnalyzer:
-    def __init__(self):
-        self._vd = SentimentIntensityAnalyzer() #analyzer object
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+# last note update: 05-10-25 @ 7:45 PM CST
+_stop = set(stopwords.words('english')) # nltk gives us some english fluff words (e.g. this, the, a, and) and groups it in a set
 
-    def sentiment(self, text: str) -> float:
-        return self._vd.polarity_scores(text)['compound'] # returns neg (negative), neu (neutral), pos (positive), compound (composite) values
+def clean_text(text: str) -> str:
+    text = re.sub(r'http\\S+|@\\w+|#', '', text) # remove URLs (http\S+), Twitter handles (@\w+), and hashtags (#) from the text.
+    text = re.sub(r'[^A-Za-z0-9\\$ ]+', '', text) # removes all characters except uppercase and lowercase letters, digits, dollar signs ($), and spaces.
+    # text -> lowercase, tokenizes into indiv words, filters out words in _stop, joins the filtered tokens back into a string
+    return ' '.join(w for w in word_tokenize(text.lower()) if w not in _stop)
 
-    def hype_score(self, likes: int, retweets: int, text: str) -> float: # returns a float; parameters are parts of a tweet and associated type
-        s = self.sentiment(text) # compound score from earlier
-        return (likes * 0.6 + retweets * 1.2) * s # formula
+def extract_tokens(text: str) -> list:
+    return re.findall(r'\\$\\w+', text.upper()) #  returns a list of tokens eg $BONK, $SOL, etc
